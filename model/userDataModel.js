945aa06fb13abcfd72c2b7890a6f4aa12a8756c5
AswalMohandas <aswalmohandas9633@gmail.com>
2026-02-04 10:01:05 +0530

    j

--- model/userDataModel.js
+++ model/userDataModel.js
@@ -16,11 +16,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  // role:{
-  //    type: String,
-  //     enum: ["user", "admin"],
-  //     default: "user",
-  // }
+  role:{
+     type: String,
+      enum: ["user", "admin"],
+      default: "user",
+  }
       
 
 
